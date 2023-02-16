06c8110d95dfea163838fe5b45f34ebec4310b48
VictrCruz312 <motoggb979@gmail.com>
2023-02-16 11:31:42 -0300

    feat: criando componente cardVehicle com estilizações e valores sendo inseridos dinâmicamente

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -2,6 +2,16 @@ import { HomeStyled } from "./styles";
 import Footer from "../../components/Footer";
 
 const Home = () => {
+  const vehicleExample = {
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum is simply dummy text of the printing",
+    title: "Product title stays here - max 1 line",
+    tags: ["0 KM", "2019"],
+    value: "00.000,00",
+    advertiserName: "Victor",
+    advertiserImage: "https://cdn-icons-png.flaticon.com/512/20/20863.png",
+  };
+
   return (
     <HomeStyled>
       <Footer />
