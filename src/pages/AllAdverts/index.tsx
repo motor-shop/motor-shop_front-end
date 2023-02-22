f55b751d72238a90cdb2a31088f393d4910da118
angelica <angelicamariazm@gmail.com>
2023-02-22 11:28:47 -0300

    feat: page allAdverts

---
+++ src/pages/AllAdverts/index.tsx
@@ -0,0 +1,74 @@
+import Header from "../../components/header";
+import Footer from "../../components/Footer";
+import Carrousel from "../../components/Carrousel";
+import CardVehicle from "../../components/CardVehicle";
+
+import { AllAdvertStyled } from "./styles";
+import { AdvertsStyled } from "./styles";
+import profile from "../../assets/profile.svg";
+
+import { useUser } from "../../Contexts/User";
+import { useAdvert } from "../../Contexts/Adverts";
+
+const AllAdverts = () => {
+  const { userMocked } = useUser();
+  const { vehiclesMocked } = useAdvert();
+
+  return (
+    <>
+      <Header />
+      <AllAdvertStyled>
+        <div className="profileContainer">
+          <img src={profile} alt="imagem de perfil" />
+          <div className="name">
+            <p className="profileName"> {userMocked.username}</p>
+            <div className="isSeller">
+              <p> Anunciante</p>
+            </div>
+          </div>
+          <p className="description">
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s
+          </p>
+        </div>
+      </AllAdvertStyled>
+      <AdvertsStyled>
+        <div className="cars">
+          <Carrousel
+            carrouselTitle="Carros"
+            gapBetweenItems="48px"
+            autoScroll={false}
+          >
+            <CardVehicle vehicle={vehiclesMocked[0]} />
+            <CardVehicle vehicle={vehiclesMocked[1]} />
+            <CardVehicle vehicle={vehiclesMocked[2]} />
+            <CardVehicle vehicle={vehiclesMocked[0]} />
+            <CardVehicle vehicle={vehiclesMocked[1]} />
+            <CardVehicle vehicle={vehiclesMocked[2]} />
+            <CardVehicle vehicle={vehiclesMocked[0]} />
+          </Carrousel>
+        </div>
+
+        <div className="motorcycles">
+          <Carrousel
+            carrouselTitle="Motos"
+            gapBetweenItems="48px"
+            autoScroll={false}
+          >
+            <CardVehicle vehicle={vehiclesMocked[0]} />
+            <CardVehicle vehicle={vehiclesMocked[1]} />
+            <CardVehicle vehicle={vehiclesMocked[2]} />
+            <CardVehicle vehicle={vehiclesMocked[0]} />
+            <CardVehicle vehicle={vehiclesMocked[1]} />
+            <CardVehicle vehicle={vehiclesMocked[2]} />
+            <CardVehicle vehicle={vehiclesMocked[0]} />
+          </Carrousel>
+        </div>
+      </AdvertsStyled>
+      <Footer />
+    </>
+  );
+};
+
+export default AllAdverts;
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,13 @@
 import { Routes, Route, Navigate } from "react-router-dom";
 import Advert from "../pages/Advert";
+import AllAdverts from "../pages/AllAdverts";
 import Home from "../pages/Home";
 
 const Routers = () => (
   <Routes>
     <Route path="/home" element={<Home />} />
     <Route path="/advert" element={<Advert />} />
+    <Route path="/allAdverts" element={<AllAdverts />} />
     <Route path="*" element={<Navigate to="/home" />} />
   </Routes>
 );
