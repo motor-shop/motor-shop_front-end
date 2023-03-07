3636556c005ec1a9050194a5c48e0d84189d0724
angelica <angelicamariazm@gmail.com>
2023-03-07 13:19:10 -0300

    feat: modal sucesso cadastro

---
+++ src/components/ModalSuccess/styles.ts
@@ -0,0 +1,22 @@
+import styled from "styled-components";
+
+export const DivStyled = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  height: 150px;
+
+  h1{
+    font-weight: 500;
+    font-size: 16px;
+    line-height: 20px;
+    color: #212529;
+  }
+
+  p {
+    font-weight: 400;
+    font-size: 16px;
+    line-height: 28px;
+    color: #495057;
+  }
+`;
